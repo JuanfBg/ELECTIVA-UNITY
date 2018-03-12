d7d93723297b77620a70d783ee633beb6b6148ba
DESKTOP-2ERUG0P\Tatiana <gutjei95@gmail.com>
2018-03-12 17:34:34 -0500

    Se añadió Escena1, Escena2, PlayerController, CameraController

---
+++ Proyecto electiva/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;//distancia entre la camara y la esfera.	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate () {
+		//Transform contiene escala, posicion y la rotacion
+		//transform.position define la posicion de la camara.
+		//player.transform.position define la posicion de la esfera
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Proyecto electiva/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	private Vector3 posicion;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();//Referencia al componente de tipo Rigidbody de Player.
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");//Captura del teclado movimiento horizontal
+		float moveVertical = Input.GetAxis("Vertical");//Captura del teclado movimiento vertical
+
+
+		Vector3 movimiento = new Vector3(moveHorizontal, 0.0f, moveVertical);//Coordenadas
+
+
+		rb.AddForce(movimiento * speed);
+	}
+}
