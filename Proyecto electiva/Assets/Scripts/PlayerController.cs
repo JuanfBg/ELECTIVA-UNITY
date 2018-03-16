2db6b125ee1f0d3a780cdf3950df38f58f321ec1
JuanfBg <bgjuan11@gmail.com>
2018-03-15 21:09:33 -0500

    Level2 Complete

--- Proyecto electiva/Assets/Scripts/PlayerController.cs
+++ Proyecto electiva/Assets/Scripts/PlayerController.cs
@@ -1,30 +1,95 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	private float time;
+    public float timePanelNivel1;
+	public float timePanelNivel2;
+    private bool banderaPanelNivel1;
+	private bool BanderaPanelNivel2;
 
-	private Rigidbody rb;
+    private Rigidbody rb;
 
 	private Vector3 posicion;
 
+	public GameObject capsulaT1;
+	public GameObject capsulaT2;
 
-	// Use this for initialization
-	void Start () {
+	private int contPuntos;
+	private int contCapsulas;
+
+    public Text textoMarcador;
+    public Text textoCronometro;
+    public Text textoPanelGanador;
+    public Text textoPanelPerdedor;
+    private Text textoSumaRestaP;
+
+    private AudioSource audioRecoleccion;
+    private AudioSource audioRecoleccionTrampa;
+
+    public Transform particulas;
+    private ParticleSystem systemaParticulas;
+
+    public float x, z;
+
+    public GameObject panelg;
+    public GameObject panelp;
+
+    // Use this for initialization
+    void Start () {
 		rb = GetComponent<Rigidbody>();//Referencia al componente de tipo Rigidbody de Player.
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+		contPuntos = 0;
+		contCapsulas = 0;
+        textoMarcador.text = "Marcador: 0";
+        textoCronometro.text = "Cronómetro: 0";
+        AudioSource[] allMyAudioSource = GetComponents<AudioSource>();
+        audioRecoleccion = allMyAudioSource[0];
+        audioRecoleccionTrampa = allMyAudioSource[1];
+        systemaParticulas = particulas.GetComponent<ParticleSystem>();
+        systemaParticulas.Stop();
+        panelg.SetActive(false);
+        panelp.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		time += Time.deltaTime;
+        textoCronometro.text = "Cronómetro: " + time.ToString("f0") ;
+
+        if (time>=60) {
+            speed = 0;
+            Debug.Log("Perdiste, se acabó el tiempo.");
+            textoCronometro.text = "Cronómetro: 60";
+            textoPanelPerdedor.text = "¡PERDISTE! Se te acabó el tiempo. Eres muy lento";
+            panelp.SetActive(true);
+        }
+
+        if (banderaPanelNivel1)
+        {
+            timePanelNivel1 -= Time.deltaTime;
+            if (timePanelNivel1 < 0)
+            {
+                //Application.LoadLevel(1);
+                SceneManager.LoadScene(1);
+
+
+
+            }
+
+
+
+        }
 
 
 
 
 
-
+    }
 
 	void FixedUpdate()
 	{
@@ -36,22 +101,72 @@ public class PlayerController : MonoBehaviour {
 
 
 		rb.AddForce(movimiento * speed);
-	}
+
+        x = transform.position.y;
 
 
-	void OnTriggerEnter(Collider other){
-		//Debug.Log ("jijijjii");
-		if (other.gameObject.CompareTag ("Recolectable")){
+        if (x < -30)
+        {
+           	Debug.Log("se cae");
+            textoPanelPerdedor.text = "¡PERDISTE! Se te cayó la bola. Eres malo con las flechas";
+            panelp.SetActive(true);
+        }
+    }
 
+	void OnTriggerEnter(Collider other)
+	{
+		//Referencia al collider al cual hemos impactado.
 
+		if (other.gameObject.CompareTag ("PlaneTrampa")) {
 			Destroy (other.gameObject);
+			capsulaT1.SetActive (false);
+		} else if(other.gameObject.CompareTag("PlaneTrampa2")){
+			Destroy (other.gameObject);
+			capsulaT2.SetActive (false);
+		} 
+		else if (other.gameObject.CompareTag ("CapsulaBuena")) {
+			contPuntos += 5;
+			contCapsulas++;
+			Destroy (other.gameObject);
+			Debug.Log ("Se suman 5 puntos. Marcador: "+contPuntos);
+            textoMarcador.text = "Marcador: "+contPuntos;
+            posicion = other.gameObject.transform.position;//Obtener la posicion del objeto que estamos colisionando.
+            particulas.position = posicion;//Asignarle 
+            systemaParticulas = particulas.GetComponent<ParticleSystem>();//Obtener el componente ParticleSystem de particulas
+            systemaParticulas.Play();
+            audioRecoleccion.Play();
+            if (contCapsulas == 11){
+                speed = 0;
+				Debug.Log ("Total puntos obtenidos: "+contPuntos);
+				Debug.Log ("Tiempo total: "+time);
+				Debug.Log ("Fin del primer nivel");
+                textoPanelGanador.text = "¡PASASTE AL SEGUNDO NIVEL!\n\nTiempo total: "+time.ToString("f0")+"\n\nPuntos totales: "+contPuntos;
+                panelg.SetActive(true);
+                timePanelNivel1 = 3;
+                banderaPanelNivel1 = true;
+            }
+		} else if (other.gameObject.CompareTag ("CapsulaMala")) {
+			contPuntos -= 2;
+			if (contPuntos < 0) {
+				Debug.Log ("No tienes puntos, no se puede descontar");
+				contPuntos = 0;
+			} else {
+				Destroy (other.gameObject);
+                textoMarcador.text = "Marcador: " + contPuntos;
+                Debug.Log ("Se restan 2 puntos. Marcador: "+contPuntos);
+                posicion = other.gameObject.transform.position;//Obtener la posicion del objeto que estamos colisionando.
+                particulas.position = posicion;//Asignarle 
+                systemaParticulas = particulas.GetComponent<ParticleSystem>();//Obtener el componente ParticleSystem de particulas
+                systemaParticulas.Play();
+                audioRecoleccionTrampa.Play();
+            }
 
-		}else{
+		} else {
+			//nada
+		}
+
+
+
+
 	}
-	
-	}
-
-
-
-
 }
